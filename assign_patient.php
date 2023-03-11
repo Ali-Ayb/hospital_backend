0e3e5bb3c01ad756e7903505e0b01e9c9f05a7ff
Ali-Ayb <aliayoub.me1@gmail.com>
2023-03-11 17:47:53 +0200

    added assign_patient api

---
+++ assign_patient.php
@@ -0,0 +1,32 @@
+<?php
+
+include 'connection_db.php';
+
+$patient = $_POST['patient'];
+$hospital = $_POST['hospital'];
+
+$user_type_id = $link->prepare('SELECT id FROM users WHERE name = ? ');
+$user_type_id->bind_param('s', $patient);
+$user_type_id->execute();
+$user_type_id->bind_result($patient_id);
+$user_type_id->fetch();
+$user_type_id->close();
+
+$user_type_id = $link->prepare('SELECT id FROM hospitals WHERE name = ? ');
+$user_type_id->bind_param('s', $hospital);
+$user_type_id->execute();
+$user_type_id->bind_result($hospital_id);
+$user_type_id->fetch();
+$user_type_id->close();
+
+$is_active = true;
+$date_joined = date("Y-m-d");
+
+
+$user_type_id = $link->prepare('Insert INTO hospital_users (hospital_id, user_id, is_active, date_joined) Value (?,?,?,?)');
+$user_type_id->bind_param('iiss', $hospital_id, $patient_id, $is_active, $date_joined);
+
+$response = ['response' => $user_type_id->execute()];
+
+
+echo json_encode($response);
