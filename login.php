ea753bddd36d046261032085b6c7d0ca077c0509
Ali-Ayb <aliayoub.me1@gmail.com>
2023-03-11 11:24:03 +0200

    added login file

---
+++ login.php
@@ -0,0 +1,47 @@
+<?php
+include "connection_db.php";
+
+require __DIR__ . '/vendor/autoload.php';
+
+use Firebase\JWT\JWT;
+
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    if (isset($_POST['email']) && isset($_POST['password'])) {
+
+        function validate($data)
+        {
+            $data = trim($data);
+            $data = stripslashes($data);
+            $data = htmlspecialchars($data);
+            return $data;
+        }
+        $email = validate($_POST['email']);
+        $password = validate($_POST['password']);
+
+        $sql = "SELECT * FROM users WHERE email='$email'";
+        $result = mysqli_query($link, $sql);
+        $response = [];
+
+
+        if (mysqli_num_rows($result) === 1) {
+            $row = mysqli_fetch_assoc($result);
+            $password = hash('sha256', $password . $row['salt']);
+
+            if ($row['email'] === $email && $row['password'] === $password) {
+
+                $payload = array(
+                    "user_id" => $row['id'],
+                    "username" => $email
+                );
+                $secret_key = "code123";
+                $jwt = JWT::encode($payload, $secret_key);
+
+                echo json_encode(array(
+                    "jwt" => $jwt
+                ));
+            }
+        }
+    }
+}
