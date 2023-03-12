2b695928fafebfbb087086bdcee441e5d1122906
Ali-Ayb <aliayoub.me1@gmail.com>
2023-03-12 17:52:35 +0200

    added jwt autharization to assign_patient

--- assign_patient.php
+++ assign_patient.php
@@ -1,6 +1,21 @@
 <?php
 
 include 'connection_db.php';
+include 'jwt.php';
+
+$headers = apache_request_headers();
+if (isset($headers['Authorization'])) {
+
+    if (!verifyToken($headers['Authorization'], $secret_key))
+        exit;
+}
+
+$user_info = user_info($headers['Authorization'], $secret_key);
+$role = $user_info->role;
+if ($role != 'admin') {
+    echo 'Not Authorized';
+    exit;
+}
 
 $patient = $_POST['patient'];
 $hospital = $_POST['hospital'];
