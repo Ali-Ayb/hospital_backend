a8fcdd9bd3443077ee72a3e9090324b523c62911
Ali-Ayb <aliayoub.me1@gmail.com>
2023-03-13 01:36:54 +0200

    added get medication api

---
+++ get_medications.php
@@ -0,0 +1,19 @@
+<?php
+include "connection_db.php";
+include 'jwt.php';
+
+$headers = apache_request_headers();
+if (isset($headers['Authorization'])) {
+
+    if (!verifyToken($headers['Authorization'], $secret_key))
+        exit;
+}
+
+
+$patient_name = $link->prepare('select name from medications');
+$patient_name->execute();
+
+$result = $patient_name->get_result();
+$rows = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+echo json_encode($rows);
