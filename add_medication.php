598511c5d33dbf9fad10a110d4982b02feb228b8
Ali-Ayb <aliayoub.me1@gmail.com>
2023-03-13 02:09:07 +0200

    fixed add medicatino api

--- add_medication.php
+++ add_medication.php
@@ -10,19 +10,20 @@ if (isset($headers['Authorization'])) {
         exit;
 }
 
-
+$user_info = user_info($headers['Authorization'], $secret_key);
+$user_id = $user_info->user_id;
 
 $medication = $_POST['medication'];
 $quantity = $_POST['quantity'];
 
-$user_type_id = $link->prepare('SELECT id FROM medications WHERE name = ? ');
+$user_type_id = $link->prepare('SELECT id FROM medicatoins WHERE name = ? ');
 $user_type_id->bind_param('s', $medication);
 $user_type_id->execute();
 $user_type_id->bind_result($medication_id);
 $user_type_id->fetch();
 $user_type_id->close();
 
-$user_type_id = $link->prepare('Insert INTO user_departments (user_id, medicatoin_id, quantity) Value (?,?,?)');
+$user_type_id = $link->prepare('Insert INTO users_has_medications (user_id, medicatoin_id, quantity) Value (?,?,?)');
 $user_type_id->bind_param('iis', $user_id, $medication_id, $quantity);
 
 $response = ['response' => $user_type_id->execute()];
